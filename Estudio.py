9805eb242c6d2aefbcff07462d1146595df70afa
Jhonatan <jhonatanhernandez@seti.com.co>
2022-04-19 10:44:31 -0500

    Estudio.py

---
+++ Estudio.py
@@ -0,0 +1,14 @@
+#veces = (int(input("escribe un numero: ")))
+#for i in range(0,veces):
+ #   print("hola")
+
+def primo():
+    for n in range(0,10):
+        if num % n == 0:
+            print("no es primo")
+        else: 
+            print("es primo")
+    
+
+num=(int(input("escriba un numero: ")))
+primo()
---
+++ EstudioFicheros.py
@@ -0,0 +1,8 @@
+try:
+    fichero = open("Estudio.py" , "r")
+except:
+    print("el programa no existe")
+else:
+    lineas = (fichero.readlines())
+    fichero.close
+    print("\n", lineas)
